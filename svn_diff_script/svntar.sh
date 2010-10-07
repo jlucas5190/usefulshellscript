3eb34e666c674367e4764a198abdbd903d6d6253
zhengkarl <zhengkarl@741f4018-093b-bec7-6201-b9e93680a0b9>
2010-10-07 08:07:57 +0000

    modified svn get modified files script; add svntar.sh script

---
+++ svn_diff_script/svntar.sh
@@ -0,0 +1,29 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE:  svntar.sh
+# 
+#         USAGE:  ./svntar.sh 
+# 
+#   DESCRIPTION:  svn modified files to tar
+#                 find the files that has modified and not commit to svn .
+#                 tar it to a tar file with filename:svn_diff_$date.tar
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR: Karl Zheng (http://blog.csdn.net/zhengkarl), ZhengKarl@gmail.com
+#       COMPANY: Meizu
+#       CREATED: 2010年10月07日 14时08分16秒 CST
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+date_str="$(date +%Y%m%d_%T|tr -d ':')"
+tmp_file_name=svn_diff_$date_str".tar" 
+svn diff | grep ^Index | awk '{printf $2 " "}END{print " "}'|xargs tar --force-local -rvf $tmp_file_name
+echo -e "\nsave the modified files to tar file: "$tmp_file_name
+unset tmp_file_namei 
+unset date_str
